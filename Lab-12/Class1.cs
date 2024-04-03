b280a759ec69da7f1b2c423cd5c2adf71edea7f8
Sharhalo <sharhalo27@gmail.com>
2024-04-03 11:56:03 +0300

    lab-4

---
+++ Lab-12/Class1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+record Person(string Name, int Total1, int Total2, int Total3);
+
+class Program
+{
+    static void Main()
+    {
+        var list = new List<Person>()
+        {
+            new Person("James", 40, 20, 40),
+            new Person("Nazar", 50, 20, 50),
+            new Person("Join", 20, 20, 50)
+        };
+
+        foreach (var person in list)
+        {
+            var (average, aboveAverageCount) = CalculateAverageAndAboveCount(person);
+            Console.WriteLine($"Ім'я: {person.Name}, Середній бал: {average:F2}, Кількість оцінок вище за середнє: {aboveAverageCount}");
+        }
+    }
+
+    static (double, int) CalculateAverageAndAboveCount(Person person)
+    {
+        var totalScores = new int[] { person.Total1, person.Total2, person.Total3 };
+        double average = totalScores.Average();
+        int aboveAverageCount = totalScores.Count(score => score > average);
+        return (average, aboveAverageCount);
+    }
+}
+
